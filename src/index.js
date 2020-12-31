6333fdbbcab551f3dbd3116378a195da9c733f94
Matviienko Artem <k2141092mav@gmail.com>
2020-12-31 15:46:07 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import './styles.css';
+
+// function digital_root(n) {
+//   if (n.length > 1) {
+//     console.log(n);
+//     // n.map((value, index) => {
+//     //   console.log(value);
+//     //   console.log(index);
+//     // });
+//   }
+// }
+
+// const proba = digital_root(164);
+// console.log(proba);
+
+/// Задача с Codewars
+
+function digitalRoot(namber) {
+  const Namber = toString(namber);
+  const stringValue = Namber.length;
+  console.log(stringValue);
+  if (stringValue > 1) {
+    for (let i = 0; i < stringValue; i += 1) {
+      console.log(namber);
+    }
+  }
+}
+
+digitalRoot(2);
